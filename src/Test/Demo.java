69ef8c42a3c028ae5b078696e09159ffcae4c014
Kritthegod <142289388+Kritthegod@users.noreply.github.com>
2023-08-17 09:37:18 +0630

    Version 2

---
+++ src/Test/Demo.java
@@ -0,0 +1,15 @@
+package Test;
+
+public class Demo {
+		//non static method
+		void display() {
+			System.out.println("Demo non_static method");
+		}
+		
+		//static method
+		public static void show() {
+			// TODO Auto-generated method stub
+			System.out.println("Demo static method");
+		}
+
+}
